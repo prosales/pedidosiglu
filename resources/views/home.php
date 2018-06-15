7373698492f5ef8997b02edf173d75ed862d8407
Pablo Rosales <prosales@researchmobile.co>
2018-06-15 13:29:41 -0600

    set logo home

--- resources/views/home.php
+++ resources/views/home.php
@@ -109,7 +109,8 @@
     </div>
 
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-      <h5 class="my-0 mr-md-auto font-weight-normal">Bienvenido, <span id="usuario"></span></h5>
+      <!-- <h5 class="my-0 mr-md-auto font-weight-normal">Bienvenido, <span id="usuario"></span></h5> -->
+      <h5 class="my-0 mr-md-auto font-weight-normal"><img class="mb-4" src="fasenco.png" alt="" width="100" height="40"></h5>
       <nav class="my-2 my-md-0 mr-md-3">
         <!-- <a class="p-2 text-dark" href="#">Features</a>
         <a class="p-2 text-dark" href="#">Enterprise</a>
